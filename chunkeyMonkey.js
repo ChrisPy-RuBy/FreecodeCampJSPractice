c0804833a2e895826901541023370f908af856a1
Chris Woodall <christopherhwoodall@gmail.com>
2017-08-25 19:46:19 +0100

    just fir the numbers

--- chunkeyMonkey.js
+++ chunkeyMonkey.js
@@ -1,11 +1,22 @@
 function chunkArrayInGroups(arr, size) {
   var subArr = [];
-  for (var i = arr.length ; i > 0; ) {
-    subArr.push(arr.slice(i-size,i));
-    i -= size;
-    console.log(subArr);
-  }
+  var clipValue = 0;
+  for (var i = 0; i > arr.length; ) {
+    if (i >= size) {
+      clipValue = i+size;
+    } else {
+      clipValue = arr.length;
+    }
+    subArr.push(arr.slice(clipValue, i));
+    i = clipValue;
+}
+  console.log (subArr.reverse());
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+chunkArrayInGroups([0, 1], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
 
